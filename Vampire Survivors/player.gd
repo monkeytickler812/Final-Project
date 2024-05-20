3f8b5abac9551c87b6700eb2f732fbb470b7a1de
nmbmonkey <jazhu8@s.sfusd.edu>
2024-05-20 11:59:48 -0700

    added mobs into main scene

--- Vampire Survivors/player.gd
+++ Vampire Survivors/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 func _physics_process(delta):
-	var direction = Input.get_vector("move_left", "move_right", "move_down", "move_up")
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
 	move_and_slide()
 	
